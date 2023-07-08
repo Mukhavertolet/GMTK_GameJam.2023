6f06f9883b2c22b34758440e2fc0216c297f26bb
Mukhavertolet <Mukhavertolet@gmail.com>
2023-07-08 18:41:21 +0300

    Camera controls (scroll, reset)

--- GMTK_GameJam.2023/GMTK_GameJam.2023/Assets/Scripts/CameraController.cs
+++ GMTK_GameJam.2023/GMTK_GameJam.2023/Assets/Scripts/CameraController.cs
@@ -17,7 +17,7 @@ public class CameraController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -25,12 +25,25 @@ public class CameraController : MonoBehaviour
     {
         translation = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
         zoomTranslation = Input.mouseScrollDelta.y;
+
+        if (Input.GetMouseButtonDown(2))
+            cam.transform.position = new Vector3(25f, 25f, -10);
+
     }
 
     private void LateUpdate()
     {
         gameObject.transform.Translate(translation * movementSpeed * Time.deltaTime);
+
+
         cam.orthographicSize -= zoomTranslation * zoomSpeed * Time.deltaTime;
+
+        if (cam.orthographicSize < 1.5f)
+            cam.orthographicSize = 1.5f;
+
+        if (cam.orthographicSize > 28f)
+            cam.orthographicSize = 28f;
+
     }
 
 
